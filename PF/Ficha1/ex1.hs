9237ec13091a1898568e51f2f8973b7cb00e71dd
Diogo Silva <100921531+diogokeeper12@users.noreply.github.com>
2023-09-12 17:35:40 +0100

    Update ex1.hs
    Ex1 Done

--- PF/Ficha1/ex1.hs
+++ PF/Ficha1/ex1.hs
@@ -2,3 +2,25 @@
 perimetro :: Float -> Float
 perimetro r = r*3.14*2
 
+--distancia entre dois pontos é sqrt((x2-x1)^2 + (y2-y1)^2)
+dist :: (Double,Double) -> (Double,Double) -> Double
+dist (x1,y1) (x2,y2) = sqrt ((x2-x1)**2 + (y2-y1)**2)
+
+primUlt :: [a] -> (a,a)
+primUlt l = (head l, last l)
+
+--um número é multiplo de outro se o resto da divisão(mod) é 0 
+multiplo :: Int -> Int -> Bool
+multiplo a b = a `mod` b == 0
+
+truncaImpar :: [a] -> [a]
+truncaImpar l | even (length l) = l
+              | otherwise = tail l
+
+max2 :: Int -> Int -> Int
+max2 a b | a > b = a
+         | otherwise = b
+
+max3 :: Int -> Int -> Int -> Int
+max3 a b c | max2 a b > c = max2 a b
+           | otherwise    = c 
