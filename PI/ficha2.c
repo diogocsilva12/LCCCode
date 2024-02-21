e5e1c4e77ce3296debc202750087b70148e7b41e
Diogo Silva <100921531+diogokeeper12@users.noreply.github.com>
2024-02-21 10:53:48 +0000

    u

--- PI/ficha2.c
+++ PI/ficha2.c
@@ -23,6 +23,27 @@ float multInt2(int n, float m){
     return res;
 }
 
+//Exercicio 3
+int mdc1(int a, int b){
+    int menor = a < b ? a:b;
+    int mdc = 1;
+    for(int i = 1;i<=menor;i++){
+        mdc = i;
+    }
+    return mdc;
+}
+
+//Exercicio 6
+//EX 6.A
+int fib1(int n){
+    if(n<2){
+        return 1;
+    }
+    else{
+        return fib1(n-1) + fib1(n-2);
+    }
+}
+
 
 int main(int argc,char* argv[]) {
     int exercicio;
@@ -49,6 +70,25 @@ int main(int argc,char* argv[]) {
             printf("Insira o segundo número: ");
             scanf("%d", &num2);
             resultado = multInt2(num1,num2);
+            printf("Resultado: %f\n",resultado);
+            break;
+        }
+        case 3:{
+            int num1 = 0;
+            int num2 = 0;
+            printf("Insira o primeiro número: ");
+            scanf("%d", &num1);
+            printf("Insira o segundo número: ");
+            scanf("%d", &num2);
+            resultado = mdc1(num1,num2);
+            printf("Resultado: %f\n",resultado);
+            break;
+        }
+        case 6: {
+            int num1 = 0;
+            printf("Insira o primeiro número: ");
+            scanf("%d", &num1);
+            resultado = fib1(num1);
             printf("Resultado: %f\n",resultado);
             break;
         }
